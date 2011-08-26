52a264aa0b2351afcd4eb38cfca6df1a311880bf
Alberto Vigata <alberto@nelalabs.com>
2011-08-26 15:19:18 -0700

    adding vgtmpeg_opts.h file

---
+++ vgtmpeg_opts.h
@@ -0,0 +1,5 @@
+    { "output_xml", OPT_BOOL, {(void*)&output_xml}, "turn on xml output" },
+    { "server_mode", OPT_BOOL, {(void*)&server_mode}, "setup server mode" },
+    { "codecs_json", OPT_EXIT, {(void*)&show_codecs_json}, "show codecs in json format" },
+    { "formats_json", OPT_EXIT, {(void*)&show_formats_json}, "show formats  in json format" },
+    { "banner", OPT_BOOL, {(void*)&banner}, "shows ffmpeg banner" },
